bd666f74b49b71e1de27e0067fc84d31e7557537
Uzair Saleem <uzairsaleemdev@gmail.com>
2023-07-24 04:27:09 +0500

    header completed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,15 +2,19 @@ import { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import styles from "./Home.module.css";
 import Typewriter from "typewriter-effect";
+import image from "../../images/uzairkhanrectangle.jpeg"
 
 export default function Home(){
 
     return(<>
-        <Navbar/>
+            <Navbar/>
+    <section className={styles.homeSection}>
+
 
         <div className={styles.container}>
+        <div className={styles.leftContainer}>
 
-        <h1 className={styles.mainHeading}>Hey! I am Uzair. I</h1>
+        <h3 className={styles.mainHeading}>Hi! I am Uzair. I...</h3>
         <div className={styles.typeContainer}>
 
         <Typewriter className={styles.typeWriter}
@@ -18,24 +22,32 @@ export default function Home(){
              onInit={(typewriter) => {
                  typewriter
                     .changeDelay(50)
-                    .typeString("am a Full-Stack Web Developer")
+                    .typeString("am a Full-Stack Web Developer.")
                      .pauseFor(1000)
                      .deleteAll()
                      .changeDelay(50)
-                     .typeString("love everything about code")
+                     .typeString("love everything about code.")
                      .pauseFor(1000)
                     .deleteAll()
                     .changeDelay(50)
-                    .typeString("also teach programming to people")
+                    .typeString("also teach programming to people.")
                     .pauseFor(1000)
                     .deleteAll()
                     .changeDelay(50)
-                    .typeString("solve problems")
+                    .typeString("solve problems.")
                     .start();
  }}
 />
         </div>
+            <button className={styles.resumeButton}>Download Resume</button>
+
         </div>
+
+        <div className={styles.rightContainer}>
+            <img className={styles.image} src={image}  alt="Profile Picture" srcset="" />
+        </div>
+</div>
+    </section>
     </>
     )
 
