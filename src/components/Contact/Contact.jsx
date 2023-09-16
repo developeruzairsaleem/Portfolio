4472c5dfee801bb40cdd8c669848a69720a218a6
Uzair Saleem <uzairsaleemdev@gmail.com>
2023-09-16 16:12:22 +0500

    Added basic project styling

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,27 +1,31 @@
-import styles from "./Contact.module.css"
+import styles from "./Contact.module.css";
 
-export default function Contact (){
-
-
-    return(
-        <>
-        <section id="contact-section" className={styles.section}>
-            <div className={styles.container}>
-                <h3 className={styles.mainHeading}>
-                    Contact Me
-                </h3>
-<div className={styles.main}>   
-
-                <input className={styles.input} placeholder="Enter your email" type="email" name="" id="" />
-                <textarea className={styles.message} placeholder="Leave a message..." name="" id="" cols="30" rows="10" ></textarea>
-                <button className={styles.submit}>Submit</button>
-</div>
-            </div>
-        </section>
-        </>
-    )
-
-
-
-
+export default function Contact() {
+  return (
+    <>
+      <section id="contact-section" className={styles.section}>
+        <div className={styles.container}>
+          <h3 className={styles.mainHeading}>Contact Me</h3>
+          <div className={styles.main}>
+            <input
+              className={styles.input}
+              placeholder="Enter your email"
+              type="email"
+              name=""
+              id=""
+            />
+            <textarea
+              className={styles.message}
+              placeholder="Leave a message..."
+              name=""
+              id=""
+              cols="30"
+              rows="10"
+            ></textarea>
+            <button className={styles.submit}>Submit</button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
